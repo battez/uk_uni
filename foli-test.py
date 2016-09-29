071602ff4478ffcb4f7654fe658ee0b9ac72d4dc
Luke B <lukebarker@gmail.com>
2016-09-29 14:12:12 +0100

    adjusted radius of circles to improve

--- foli-test.py
+++ foli-test.py
@@ -30,7 +30,7 @@ map_osm = folium.Map(location=[53.5074, -0.1 ], zoom_start=zoom_start)
 output_osm = 'unis_clustered.html'
 marker_cluster = folium.MarkerCluster("UK Universities and Student Numbers").add_to(map_osm)
 
-map_alt = folium.Map(location=[53.5074, -0.1 ], zoom_start=zoom_start)
+map_alt = folium.Map(location=[53.5074, -0.1 ], zoom_start=7)
 output_alt = 'unis_relative.html'
 
 # import data
@@ -40,7 +40,7 @@ with open(locs_uk_unis, 'rb') as csvfile:
 
     # set up default Folium marker values
     radius = 20
-    scale_value = 1000 * 8
+    scale_value = 5
     population = ' N/A'
     fill_color = '#33bb33'
     
@@ -91,7 +91,7 @@ with open(locs_uk_unis, 'rb') as csvfile:
         # and make a alt. map circle marker, with radius rel. to population:
         folium.CircleMarker(location=[row['lat'], row['lon']], \
             radius=radius, popup=popup, \
-            fill_color=fill_color, fill_opacity=0.5).add_to(map_alt)
+            fill_color=fill_color, fill_opacity=0.25).add_to(map_alt)
         
 
     # output both maps
